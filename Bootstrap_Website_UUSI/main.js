8aa657d0e05165bddc6277b20e20630b460c9de1
Liisa Pikkarainen <liisapikkarainen1@gmail.com>
2021-12-13 00:04:33 +0200

    Edistymistä

--- Bootstrap_Website_UUSI/main.js
+++ Bootstrap_Website_UUSI/main.js
@@ -6,4 +6,7 @@ ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
 
-
+var alertList = document.querySelectorAll('.alert')
+var alerts =  [].slice.call(alertList).map(function (element) {
+  return new bootstrap.Alert(element)
+})
